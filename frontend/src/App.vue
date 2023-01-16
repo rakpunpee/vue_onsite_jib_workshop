8ee148d21877859217fb026170a508753fd0aa5a
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-16 11:58:56 +0700

    1234

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,17 +2,17 @@
   <div>
     <Header />
     <Menu />
-    <RouterView />
+    <Content />
   </div>
 </template>
 
 <script lang="ts">
 import Header from "@/components/core/Header.vue";
 import Menu from "@/components/core/Menu.vue";
-import Login from "@/views/Login.vue";
+import Content from "@/components/core/Content.vue";
 
 export default {
-  components: { Header, Menu, Login },
+  components: { Header, Menu, Content },
   setup() {},
 };
 </script>
