ca687527dacc94a1a14d08181239f6e2c4981048
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-16 11:27:04 +0700

    1234

---
+++ frontend/src/components/core/Content.vue
@@ -0,0 +1,7 @@
+<template>Header</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style></style>
---
+++ frontend/src/components/core/Menu.vue
@@ -0,0 +1,7 @@
+<template>Menu</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style></style>
