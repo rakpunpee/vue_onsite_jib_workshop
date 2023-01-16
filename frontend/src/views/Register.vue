e4775eff9780dd8d3632b9bbacae977723fce12a
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-16 15:14:24 +0700

    1234

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,7 +1,94 @@
-<template>Register</template>
+<template>
+  <a-card
+    hoverable
+    style="
+      box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 5px 0px;
+      width: 300px;
+      height: 500px;
+    "
+  >
+    <template #cover>
+      <img alt="example" src="@/assets/03-vuejs-login.png" />
+    </template>
 
+    <h1 className="text-2xl font-bold pb-3">Register</h1>
+
+    <a-form
+      layout="vertical"
+      :model="formState"
+      @finish="handleFinish"
+      @finishFailed="handleFinishFailed"
+    >
+      <a-form-item>
+        <a-input v-model:value="formState.username" placeholder="Username">
+          <template #prefix
+            ><UserOutlined style="color: rgba(0, 0, 0, 0.25)"
+          /></template>
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input
+          v-model:value="formState.password"
+          type="password"
+          placeholder="Password"
+        >
+          <template #prefix
+            ><LockOutlined style="color: rgba(0, 0, 0, 0.25)"
+          /></template>
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-space direction="vertical" style="width: 100%" size="small">
+          <a-button block type="primary" html-type="submit">
+            Register
+          </a-button>
+
+          <a-button
+            block
+            type="default"
+            html-type="button"
+            @click="$router.push('/login')"
+          >
+            Cancel
+          </a-button>
+        </a-space>
+      </a-form-item>
+    </a-form>
+  </a-card>
+</template>
 <script lang="ts">
-export default {};
-</script>
+import { UserOutlined, LockOutlined } from "@ant-design/icons-vue";
+import { defineComponent, reactive } from "vue";
+import type { UnwrapRef } from "vue";
+import type { FormProps } from "ant-design-vue";
+// import api from "@/services/api";
+// import type { User } from "@/models/user.model";
 
-<style></style>
+interface FormState {
+  username: string;
+  password: string;
+}
+export default defineComponent({
+  components: {
+    UserOutlined,
+    LockOutlined,
+  },
+  setup() {
+    const formState: UnwrapRef<FormState> = reactive<any>({
+      username: "",
+      password: "",
+    });
+    const handleFinish: FormProps["onFinish"] = async (values) => {
+      //   await api.register(formState);
+    };
+    const handleFinishFailed: FormProps["onFinishFailed"] = (errors) => {
+      alert(JSON.stringify(errors));
+    };
+    return {
+      formState,
+      handleFinish,
+      handleFinishFailed,
+    };
+  },
+});
+</script>
