254f4e607871b074c34f2badb10bff2f9a9d2f04
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-16 10:11:56 +0700

    1234

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1>JIB</h1>
+  <div>
+    <h1>JIB</h1>
+    <div>Tmp1: {{ tmp1 }}</div>
+  </div>
 </template>
 
 <script lang="ts">
-export default {};
+export default {
+  setup() {
+    const tmp1 = "lek";
+    return { tmp1 };
+  },
+};
 </script>
 
 <style></style>
