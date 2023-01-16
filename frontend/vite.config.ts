8750b0fbbb760c0c3ad1ff50c8f4a203b7b879f0
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-16 13:17:36 +0700

    1234

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,19 @@ import { fileURLToPath } from "node:url";
 export default defineConfig({
   server: { port: 4200 },
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          "primary-color": "#1DA57A",
+          "link-color": "#1DA57A",
+          "border-radius-base": "10px",
+          "table-row-hover-bg": "#1DA57A11",
+        },
+        javascriptEnabled: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
