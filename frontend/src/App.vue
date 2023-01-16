d92f0ab350b64f8269e6f20695fb5b555054c5dc
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-16 10:16:35 +0700

    1234

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,10 @@
   <div>
     <h1>JIB</h1>
     <div>Tmp1: {{ tmp1 }}</div>
+    <div>Tmp2: {{ tmp2 }}</div>
+    <div>Tmp3: {{ tmp3 }}</div>
+    <button v-on:click="handle1">Click1</button>
+    <button @click="handle1">Click1</button>
   </div>
 </template>
 
@@ -9,7 +13,14 @@
 export default {
   setup() {
     const tmp1 = "lek";
-    return { tmp1 };
+    const tmp2 = 1;
+    const tmp3 = true;
+
+    function handle1() {
+      alert("Hey1");
+    }
+
+    return { tmp1, tmp2, tmp3, handle1 };
   },
 };
 </script>
