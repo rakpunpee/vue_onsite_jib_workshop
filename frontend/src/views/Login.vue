9c7a2ee6df9276196441ccfa9bf07cf7caab8a93
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-16 13:54:13 +0700

    1234

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,90 +1,18 @@
 <template>
-  <h1 class="bg-blue-600 text-white rounded-[100px] mt-[300px]">
-    Hello world!
-  </h1>
-
-  <a-button type="primary" shape="circle">
-    <template #icon><SearchOutlined /></template>
-  </a-button>
-  <a-button type="primary" shape="circle">A</a-button>
-  <a-button type="primary">
-    <template #icon><SearchOutlined /></template>
-    Search
-  </a-button>
-  <a-button shape="circle">
-    <template #icon><SearchOutlined /></template>
-  </a-button>
-  <a-button>
-    <template #icon><SearchOutlined /></template>
-    Search
-  </a-button>
-  <a-button shape="circle">
-    <template #icon><SearchOutlined /></template>
-  </a-button>
-  <a-button>
-    <template #icon><SearchOutlined /></template>
-    Search
-  </a-button>
-  <a-button type="dashed" shape="circle">
-    <template #icon><SearchOutlined /></template>
-  </a-button>
-  <a-button type="dashed">
-    <template #icon><SearchOutlined /></template>
-    Search
-  </a-button>
-  <a-button href="https://www.google.com">
-    <template #icon><SearchOutlined /></template>
-  </a-button>
-  <br />
-  <br />
-  <a-tooltip title="search">
-    <a-button type="primary" shape="circle" size="large">
-      <template #icon><SearchOutlined /></template>
-    </a-button>
-  </a-tooltip>
-  <a-button type="primary" shape="circle" size="large">A</a-button>
-  <a-button type="primary" size="large">
-    <template #icon><SearchOutlined /></template>
-    Search
-  </a-button>
-  <a-tooltip title="search">
-    <a-button shape="circle" size="large">
-      <template #icon><SearchOutlined /></template>
-    </a-button>
-  </a-tooltip>
-  <a-button size="large">
-    <template #icon><SearchOutlined /></template>
-    Search
-  </a-button>
-  <br />
-  <a-tooltip title="search">
-    <a-button shape="circle" size="large">
-      <template #icon><SearchOutlined /></template>
-    </a-button>
-  </a-tooltip>
-  <a-button size="large">
-    <template #icon><SearchOutlined /></template>
-    Search
-  </a-button>
-  <a-tooltip title="search">
-    <a-button type="dashed" shape="circle" size="large">
-      <template #icon><SearchOutlined /></template>
-    </a-button>
-  </a-tooltip>
-  <a-button type="dashed" size="large">
-    <template #icon><SearchOutlined /></template>
-    Search
-  </a-button>
-  <a-button size="large" href="https://www.google.com">
-    <template #icon><search-outlined /></template>
-  </a-button>
+  <div></div>
 </template>
 
 <script lang="ts">
-import { SearchOutlined } from "@ant-design/icons-vue";
-
 export default {
-  components: { SearchOutlined },
+  setup() {
+    let count = 0;
+
+    const add = () => {
+      count = count + 1;
+    };
+
+    return { count, add };
+  },
 };
 </script>
 
