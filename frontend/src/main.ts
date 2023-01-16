97bcab5764e8cc36fe43c8002ade905f0fd476e9
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-16 13:10:47 +0700

    1234

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,11 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "@/router/index";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css"; // or 'ant-design-vue/dist/antd.less'
+import "ant-design-vue/dist/antd.less";
 
 const app = createApp(App);
+app.use(Antd);
 app.use(router);
 app.mount("#app");
