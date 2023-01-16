c6695fea128bb254d52021b0363ea238ffd621af
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-16 14:53:52 +0700

    1234

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
-    <form>
-      <input type="text" placeholder="Username" :value="user.username" /><br />
-      <input type="text" placeholder="Password" :value="user.password" /><br />
+    <a-form>
+      <a-input
+        type="text"
+        placeholder="Username"
+        :value="user.username"
+      /><br />
+      <a-input
+        type="text"
+        placeholder="Password"
+        :value="user.password"
+      /><br />
       <span>Debug: {{ user }}</span>
       <br />
       <button type="submit">Submit</button>
       <button type="button" @click="handleClear">Clear</button>
-    </form>
+    </a-form>
   </div>
 </template>
 
 <script lang="ts">
 import { UserOutlined, LockOutlined } from "@ant-design/icons-vue";
-import { defineComponent, reactive, computed } from "vue";
+import { defineComponent, reactive, computed, ref } from "vue";
 import type { UnwrapRef } from "vue";
 import type { FormProps } from "ant-design-vue";
 // import type { User } from "@/models/user.model";
@@ -30,17 +38,19 @@ export default {
   },
 
   setup() {
-    const user = reactive<any>({
+    const user = reactive<FormState>({
       username: "admin",
       password: "1234",
     });
+
+    const username = ref("");
 
     const handleClear = () => {
       user.username = "";
       user.password = "";
     };
 
-    return { user, handleClear };
+    return { user, handleClear, username };
   },
 };
 </script>
