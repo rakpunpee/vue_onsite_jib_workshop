33fa7978a5fc42bfa4b1810ab743ef421a0f2f0c
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-17 14:34:33 +0700

    1234

--- frontend/src/components/core/Header.vue
+++ frontend/src/components/core/Header.vue
@@ -1,9 +1,58 @@
 <template>
-  <div>Header</div>
+  <a-layout-header :style="{ background: '#fff', padding: 0 }">
+    <upload-outlined />
+  </a-layout-header>
 </template>
-
 <script lang="ts">
-export default {};
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from "@ant-design/icons-vue";
+import { defineComponent, ref } from "vue";
+
+import Menu from "@/components/core/Menu.vue";
+export default defineComponent({
+  components: {
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    Menu,
+  },
+  setup() {
+    const onCollapse = (collapsed: boolean, type: string) => {
+      console.log(collapsed, type);
+    };
+
+    const onBreakpoint = (broken: boolean) => {
+      console.log(broken);
+    };
+
+    return {
+      selectedKeys: ref<string[]>(["4"]),
+      onCollapse,
+      onBreakpoint,
+    };
+  },
+});
 </script>
 
-<style></style>
+<style>
+.logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+
+.site-layout-sub-header-background {
+  background: #fff;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+
+[data-theme="dark"] .site-layout-sub-header-background {
+  background: #141414;
+}
+</style>
