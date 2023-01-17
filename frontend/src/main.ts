b8543d6a1acd1d5390eb6b9b2a99a7f1fcecebb4
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-17 15:30:14 +0700

    1234

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from "vue";
-import "./style.css";
+// import "./style.css";
 import App from "./App.vue";
 import router from "@/router/index";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css"; // or 'ant-design-vue/dist/antd.less'
 import "ant-design-vue/dist/antd.less";
+import { createPinia } from "pinia";
 
+const pinia = createPinia();
 const app = createApp(App);
+app.use(pinia);
 app.use(Antd);
 app.use(router);
 app.mount("#app");
