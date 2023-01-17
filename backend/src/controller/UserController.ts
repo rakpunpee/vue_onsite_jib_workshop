b32db70717b1f9f54b28f39bc46b49f91c2b5622
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-17 10:54:05 +0700

    1234

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from "express";
 import { Users } from "../entity/Users";
 
 export class UserController {
-  private userRepository = AppDataSource.getMongoRepository(User);
+  private userRepository = AppDataSource.getMongoRepository(Users);
 
   async all(request: Request, response: Response, next: NextFunction) {
     return this.userRepository.find();
