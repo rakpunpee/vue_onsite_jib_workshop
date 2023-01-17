e7d41f65b7aca57209c109ed6a4ba4d3509ebe63
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-17 10:39:17 +0700

    1234

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -5,6 +5,8 @@ import { User } from "./entity/User";
 export const AppDataSource = new DataSource({
   type: "mongodb",
   database: "demo",
+  host: process.env.MONGO_HOST ? process.env.MONGO_HOST : "localhost",
+  port: process.env.MONGO_PORT ? Number(process.env.MONGO_PORT) : 27018,
   synchronize: true,
   logging: false,
   entities: [User],
