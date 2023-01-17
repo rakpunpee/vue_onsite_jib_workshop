7efdb89121347aef2223cb810a5635b460d66bb0
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-17 15:37:52 +0700

    1234

--- frontend/src/stores/useCounterStore.ts
+++ frontend/src/stores/useCounterStore.ts
@@ -1,3 +1,15 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useCounterStore = defineStore("counterStore", () => {});
+export const useCounterStore = defineStore("counterStore", () => {
+  const count = ref(0);
+  function add() {
+    count.value++;
+  }
+
+  function remove() {
+    count.value--;
+  }
+
+  return { count, add, remove };
+});
